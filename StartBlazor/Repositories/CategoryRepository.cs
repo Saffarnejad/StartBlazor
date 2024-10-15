d054f08ff60e02c8f328b3e43a7eddaffba16e2f
Amin Saffarnejad <vira1368@gmail.com>
2024-10-15 23:52:20 +0330

    Create ICategoryRepository and implement Category Repository

---
+++ StartBlazor/Repositories/CategoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartBlazor.Data;
+using StartBlazor.Repositories.Contracts;
+
+namespace StartBlazor.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                return (await _context.SaveChangesAsync()) > 0;
+            }
+            return false;
+        }
+
+        public async Task<Category> GetAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category != null)
+            {
+                return category;
+            }
+            return new Category();
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            var categoryInDb = await _context.Categories.FirstOrDefaultAsync(c => c.Id == category.Id);
+            if (categoryInDb != null)
+            {
+                categoryInDb.Name = category.Name;
+                _context.Categories.Update(categoryInDb);
+                await _context.SaveChangesAsync();
+                return category;
+            }
+            return new Category();
+        }
+    }
+}
---
+++ StartBlazor/Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using StartBlazor.Data;
+
+namespace StartBlazor.Repositories.Contracts
+{
+    public interface ICategoryRepository
+    {
+        public Task<Category> CreateAsync(Category category);
+        public Task<Category> UpdateAsync(Category category);
+        public Task<bool> DeleteAsync(int id);
+        public Task<Category> GetAsync(int id);
+        public Task<IEnumerable<Category>> GetAllAsync();
+    }
+}
