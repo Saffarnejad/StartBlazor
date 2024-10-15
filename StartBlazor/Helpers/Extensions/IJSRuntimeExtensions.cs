56bd693aee8a1446cf11f23bb81d52b7dbbf3423
Amin Saffarnejad <vira1368@gmail.com>
2024-10-16 00:15:49 +0330

    Add IJSRuntimeExtensions.

---
+++ StartBlazor/Helpers/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+
+namespace StartBlazor.Helpers.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task Toastr(this IJSRuntime js, ToastrType type, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", type.ToString().ToLower(), message);
+        }
+    }
+
+    public enum ToastrType
+    {
+        Success = 0,
+        Warning = 1,
+        Error = 2,
+        Info = 3
+    }
+}
