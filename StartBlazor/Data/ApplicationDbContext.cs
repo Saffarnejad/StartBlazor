2659890f77cd09a25d308bf7a7129dadaebd980a
Amin Saffarnejad <vira1368@gmail.com>
2024-10-23 21:46:24 +0330

    Create Orders and OrderItems tables.

--- StartBlazor/Data/ApplicationDbContext.cs
+++ StartBlazor/Data/ApplicationDbContext.cs
@@ -8,6 +8,8 @@ namespace StartBlazor.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
