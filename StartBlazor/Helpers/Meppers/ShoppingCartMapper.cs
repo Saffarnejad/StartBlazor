c0dd8bb5726c5482f69debcb6b7a723b31f9f932
Amin Saffarnejad <vira1368@gmail.com>
2024-10-24 21:44:18 +0330

    Add ShoppingCartMapper.

---
+++ StartBlazor/Helpers/Meppers/ShoppingCartMapper.cs
@@ -0,0 +1,20 @@
+﻿using StartBlazor.Data;
+
+namespace StartBlazor.Helpers.Meppers
+{
+    public static class ShoppingCartMapper
+    {
+        public static List<OrderItem> ToOrderItems(List<ShoppingCart> shoppingCarts)
+        {
+            return shoppingCarts
+                .Select(shoppingCart => new OrderItem
+                {
+                    ProductId = shoppingCart.ProductId,
+                    Count = shoppingCart.Count,
+                    Price = shoppingCart.Product.Price,
+                    ProductName = shoppingCart.Product.Name
+                })
+                .ToList();
+        }
+    }
+}
