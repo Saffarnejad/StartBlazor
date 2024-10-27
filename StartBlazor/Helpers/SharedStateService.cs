0bd17889f131e3a58203826f3512adc688e56122
Amin Saffarnejad <vira1368@gmail.com>
2024-10-28 03:00:36 +0330

    Add SharedStateService class.

---
+++ StartBlazor/Helpers/SharedStateService.cs
@@ -0,0 +1,20 @@
+﻿namespace StartBlazor.Helpers
+{
+    public class SharedStateService
+    {
+        public event Action OnChange;
+        private int _totalShoppingCartCount;
+
+        public int TotalShoppingCartCount
+        {
+            get => _totalShoppingCartCount;
+            set
+            {
+                _totalShoppingCartCount = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
